381a09b852bf67c4ae1f157063cafefaa5c5000a
Franaffel <franksbass19@gmail.com>
2026-05-26 23:29:59 +0800

    adj

--- mophead/Mophead/pinkbullet.gd
+++ mophead/Mophead/pinkbullet.gd
@@ -2,10 +2,10 @@ extends Area2D
 
 var direction = Vector2.RIGHT
 
-const SPEED = 950.0
+const SPEED = 550.0
 const DAMAGE = 1
 
-const fire_rate = 0.12
+const fire_rate = 0.28
 
 
 func _process(delta):
--- mophead/main.gd
+++ mophead/main.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$HPLabel.text = "HP: " + str($Mophead.HP)
+	$CanvasLayer/HPLabel.text = "HP: " + str($Mophead.HP)
